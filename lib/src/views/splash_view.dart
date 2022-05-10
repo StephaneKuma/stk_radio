f1956ee775dc898de4e94594a019f80b80278fc8
Kuma Stéphane <kumastephane@gmail.com>
2022-05-10 00:23:41 +0000

    Splash view added

---
+++ lib/src/views/splash_view.dart
@@ -0,0 +1,125 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+import 'home_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with TickerProviderStateMixin {
+  double _fontSize = 2;
+  double _containerSize = 1.5;
+  double _textOpacity = 0.0;
+  double _containerOpacity = 0.0;
+
+  late AnimationController _controller;
+  late Animation<double> animation1;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    animation1 = Tween<double>(begin: 40, end: 20).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastLinearToSlowEaseIn,
+      ),
+    )..addListener(() {
+        _textOpacity = 1.0;
+      });
+
+    _controller.forward();
+
+    Timer(const Duration(seconds: 2), () {
+      setState(() {
+        _containerSize = 2;
+        _containerOpacity = 1;
+      });
+    });
+
+    Timer(const Duration(seconds: 4), () {
+      setState(() {
+        Navigator.pushReplacement(
+          context,
+          PageTransition(
+            type: PageTransitionType.scale,
+            alignment: Alignment.bottomCenter,
+            child: const HomeView(),
+          ),
+        );
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 2000),
+                curve: Curves.fastLinearToSlowEaseIn,
+                height: _height / _fontSize,
+              ),
+              AnimatedOpacity(
+                opacity: _textOpacity,
+                duration: const Duration(milliseconds: 1000),
+                child: Text(
+                  'STK RADIO',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: animation1.value,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Center(
+            child: AnimatedOpacity(
+              duration: const Duration(milliseconds: 2000),
+              curve: Curves.fastLinearToSlowEaseIn,
+              opacity: _containerOpacity,
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 2000),
+                curve: Curves.fastLinearToSlowEaseIn,
+                height: _width / _containerSize,
+                width: _width / _containerSize,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                // child: Image.asset('assets/images/logo.png'),
+                child: const Text('STK'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
